7d3394e196367cd629ad2a233d9fbe011eea0293
zhangyiwen <zzyyyww9595@gmail.com>
2019-07-10 17:21:07 +0800

    fmt

--- src/util.cc
+++ src/util.cc
@@ -69,8 +69,7 @@ Slice Compress(const CompressionContext& ctx, const Slice& input,
         return *output;
       }
       break;
-    default: {
-    }  // Do not recognize this compression type
+    default: {}  // Do not recognize this compression type
   }
 
   // Compression method is not supported, or not good compression
