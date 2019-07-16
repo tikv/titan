3108951f2d78ddd39458c55aee2704362f4a5c56
wujy-cs <wujy.cs@gmail.com>
2019-07-16 14:13:46 +0800

    fix

--- src/blob_file_iterator_test.cc
+++ src/blob_file_iterator_test.cc
@@ -209,6 +209,7 @@ TEST_F(BlobFileIteratorTest, MergeIterator) {
   BlobFileMergeIterator iter(std::move(iters));
 
   iter.SeekToFirst();
+  int i = 1;
   for (;iter.Valid(); i++, iter.Next()) {
     ASSERT_OK(iter.status());
     ASSERT_TRUE(iter.Valid());
