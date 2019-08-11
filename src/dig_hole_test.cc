568180b30530298c4ba2826709da31e861463a3d
lhy1024 <liuhanyang@pingcap.com>
2019-08-11 14:58:36 +0800

    add more unit test;

--- src/dig_hole_test.cc
+++ src/dig_hole_test.cc
@@ -12,7 +12,13 @@
 #include "util/testharness.h"
 namespace rocksdb {
 namespace titandb {
-
+const uint64_t kRandomMax = 1000;
+const uint64_t kTestStep = 10;
+uint64_t Random() {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return tv.tv_usec % kRandomMax;
+}
 class DigHoleTest : public testing::Test {
  public:
   Env *env_{Env::Default()};
@@ -29,6 +35,12 @@ class DigHoleTest : public testing::Test {
   std::shared_ptr<DigHoleJob> dig_hole_job_;
   std::unordered_map<std::string, BlobHandle *> data_;
   const uint64_t kKeyLength = 8;
+  const uint64_t kBlobMaxFileSize = 256U << 20;
+  const uint64_t kValueMaxLength = 32U << 10;
+  const uint64_t kRecordNum = 1000;
+  const uint64_t kBlockSize = 4096;
+  uint64_t expect_before_size;
+  uint64_t expect_after_size;
 
   DigHoleTest() : dirname_(test::TmpDir(env_)) {
     titan_options_.dirname = dirname_;
@@ -36,7 +48,7 @@ class DigHoleTest : public testing::Test {
     file_name_ = BlobFileName(dirname_, file_number_);
     dig_hole_job_ = std::make_shared<DigHoleJob>(
         titan_options_, env_options_, env_, titan_cf_options_,
-        std::bind(&DigHoleTest::IsShutingDown, this),
+        std::bind(&DigHoleTest::IsShutingDown),
         std::bind(&DigHoleTest::DiscardEntry, this, std::placeholders::_1,
                   std::placeholders::_2, std::placeholders::_3));
   }
@@ -46,13 +58,13 @@ class DigHoleTest : public testing::Test {
     env_->DeleteDir(dirname_);
   }
 
-  std::string GenKey(uint64_t i) {
-    char buf[64];
-    snprintf(buf, sizeof(buf), "k-%08" PRIu64, i);
-    return buf;
+  static bool IsShutingDown() { return false; }
+  Status DiscardEntry(const Slice &key, const BlobIndex &blob_index,
+                      bool *discardable) {
+    std::string key_str = DecodeKey(key);
+    *discardable = (data_.find(key_str) == data_.end());
+    return Status::OK();
   }
-
-  std::string GenValue(uint64_t len) { return std::string(len, 'v'); }
 
   void GetRealSize(u_int64_t *size) {
     TitanDBOptions db_options(titan_options_);
@@ -63,6 +75,60 @@ class DigHoleTest : public testing::Test {
     assert(s.ok());
     s = file->GetSizeOnDisk(size);
     assert(s.ok());
+  }
+
+  void GetExpectBeforeSize(const BlobHandle &handle) {
+    expect_before_size =
+        ((handle.offset + handle.size - 1) / kBlockSize + 1) * kBlockSize +
+        kBlockSize /*foot*/;
+    assert(expect_before_size % kBlockSize == 0);
+  }
+
+  void GetExpectAfterSize() {
+    int64_t blocks_num = expect_before_size / kBlockSize;
+    std::vector<uint64_t> flags(blocks_num, 0);
+    flags[0] = flags[blocks_num - 1] = 1;  // head and foot
+    for (const auto &iter : data_) {
+      uint64_t start = (iter.second->offset) / kBlockSize;
+      uint64_t end = (iter.second->offset + iter.second->size) / kBlockSize;
+      for (uint64_t i = start; i <= end; ++i) {
+        flags[i] = 1;
+      }
+    }
+    uint64_t count = 0;
+    for (const auto &i : flags) {
+      if (i) {
+        count++;
+      }
+    }
+    expect_after_size = count * kBlockSize;
+  }
+
+  std::string EncodeKey(const std::string &key) {
+    int64_t remain =
+        static_cast<int64_t>(kKeyLength) - static_cast<int64_t>(key.length());
+    assert(remain >= 0);
+    return std::string(remain, '0') + key;
+  }
+
+  std::string DecodeKey(const Slice &key) {
+    std::string data = std::string(key.data());
+    return data.substr(0, kKeyLength);
+  }
+
+  void AddKeyValue(const std::string &key, const std::string &value,
+                   BlobHandle *blob_handle) {
+    BlobRecord record;
+    record.key = EncodeKey(key);
+    record.value = value;
+    builder_->Add(record, blob_handle);
+    ASSERT_OK(builder_->status());
+    data_.insert({record.key.data(), blob_handle});
+  }
+
+  void DelKeyValue(const std::string &key) {
+    std::string key_str = EncodeKey(key);
+    data_.erase(key_str);
   }
 
   void NewBuilder() {
@@ -77,82 +143,89 @@ class DigHoleTest : public testing::Test {
     }
     builder_.reset(
         new BlobFileBuilder(db_options, cf_options, writable_file_.get()));
+    data_.clear();
   }
-
-  void AddKeyValue(const std::string &key,
-                   const std::string &value,  // uint64_t value_len,
-                   BlobHandle *blob_handle) {
-    BlobRecord record;
-    int remain = kKeyLength - static_cast<int64_t>(key.length());
-    assert(remain >= 0);
-    record.key = std::string(remain, '0') + key;
-    record.value = value;  // GenValue(value_len);
-    builder_->Add(record, blob_handle);
-    ASSERT_OK(builder_->status());
-    data_.insert({record.key.data(), blob_handle});
-  }
-
-  void DelKeyValue(const std::string &key) { data_.erase(key); }
 
   void FinishBuilder() {
     ASSERT_OK(builder_->Finish());
     ASSERT_OK(builder_->status());
   }
 
-  bool IsShutingDown() { return false; }
-  Status DiscardEntry(const Slice &key, const BlobIndex &blob_index,
-                      bool *discardable) {
-    std::string data = std::string(key.data());
-    std::string key_str = data.substr(0, kKeyLength);
-    *discardable = (data_.find(key_str) == data_.end());
-    return Status::OK();
-  }
-
-  void testDig(uint64_t expect_before_size, uint64_t expect_after_size) {
+  void CheckKeyExists() {
+    // open file
     uint64_t file_size = 0;
     ASSERT_OK(env_->GetFileSize(file_name_, &file_size));
-
-    uint64_t before_size = 0;
-    GetRealSize(&before_size);
-    ASSERT_EQ(before_size, expect_before_size);
-
-    BlobFileMeta blob_file_meta(file_number_, file_size);
-    dig_hole_job_->Exec(&blob_file_meta);
-
-    uint64_t after_size = 0;
-    GetRealSize(&after_size);
-    ASSERT_EQ(after_size, expect_after_size);
-    // LOG(INFO) << "before: " << before_size << " after: " << after_size;
+    OpenBlobFile(file_number_, 0, titan_options_, env_options_, env_,
+                 &random_rw_file_);
+    blob_file_iterator_.reset(new BlobFileIterator{
+        std::move(random_rw_file_), file_number_, file_size, TitanCFOptions()});
+    // get keys in file
+    std::unordered_set<std::string> keys_in_file;
+    for (blob_file_iterator_->SeekToFirst(); blob_file_iterator_->Valid();
+         blob_file_iterator_->Next()) {
+      ASSERT_OK(blob_file_iterator_->status());
+      std::string key_str = DecodeKey(blob_file_iterator_->key());
+      keys_in_file.insert(key_str);
+    }
+    // data_ should be subset of keys_in_file
+    for (auto &iter : data_) {
+      bool ans = keys_in_file.find(iter.first) != keys_in_file.end();
+      if (!ans) {
+        LOG(INFO) << iter.first;
+        assert(ans);
+      }
+    }
   }
-
-  uint64_t GetInitOffset(const BlobHandle &handle) {
-    return ((handle.offset + handle.size - 1) / 4096 + 1) * 4096 +
-           4096 /*foot*/;
-  }
-
-  void testTotalData(bool enable_del) {
+  //Write random number and random length records to blob file, delete random number records and dig.
+  //Test size before dig and size after dig, and then check remain key in the file.
+  void Test(uint64_t threshold_discard) {
     NewBuilder();
-
-    Random random(19260817);//todo add real random
-    const int n = random.Uniform(1000);
+    // add records
+    int n = Random() * kRecordNum / kRandomMax + 1;
     std::vector<BlobHandle> handles(n);
     for (int i = 0; i < n; i++) {
       auto id = std::to_string(i);
-      AddKeyValue(id, GenValue(i + 1), &handles[i]);
+      std::string value =
+          std::string(Random() * kValueMaxLength / kRandomMax, 'v');
+      AddKeyValue(id, value, &handles[i]);
+      assert(handles[i].offset < kBlobMaxFileSize);
     }
     FinishBuilder();
-    int64_t blob_size = GetInitOffset(handles[n - 1]);
-    if(enable_del){
-      data_.clear();
-      testDig(blob_size, 8192);  // head+foot
-    }else{
-      testDig(blob_size, blob_size);
+    GetExpectBeforeSize(handles[n - 1]);
+    // del records
+    for (int i = 0; i < n; i++) {
+      auto id = std::to_string(i);
+      if (Random() > threshold_discard) {
+        DelKeyValue(id);
+      }
     }
+    // check before size
+    uint64_t file_size = 0;
+    ASSERT_OK(env_->GetFileSize(file_name_, &file_size));
+    uint64_t before_size = 0;
+    GetRealSize(&before_size);
+    ASSERT_EQ(before_size, expect_before_size);
+    // dig
+    BlobFileMeta blob_file_meta(file_number_, file_size);
+    Status s = dig_hole_job_->Exec(&blob_file_meta);
+    assert(s.ok());
+    // check after size
+    uint64_t after_size = 0;
+    GetRealSize(&after_size);
+    assert(before_size >= after_size);
+    GetExpectAfterSize();
+    ASSERT_EQ(after_size, expect_after_size);
+    // check
+    CheckKeyExists();
   }
 };
 
-TEST_F(DigHoleTest, testDelNone) { testTotalData(true); }
-TEST_F(DigHoleTest, testDelAll) { testTotalData(false); }
+TEST_F(DigHoleTest, Test) {
+  assert(kRandomMax%kTestStep==0);
+  for (uint64_t i = 0; i <= kRandomMax; i+=kTestStep) {
+    Test(i);
+  }
+}
 
 }  // namespace titandb
 }  // namespace rocksdb
