6d34f6f52f909285aeff80b2cd2f002c449edb2f
Wu Jiayu <wujy.cs@gmail.com>
2019-09-07 14:20:31 +0800

    fmt

--- src/blob_format.h
+++ src/blob_format.h
@@ -135,11 +135,11 @@ struct BlobIndex {
 
 // Format of blob file meta (not fixed size):
 //
-//    +-------------+-----------+--------------+-------------+--------------------+--------------------+
-//    | file number | file size | file entries |  file level |    smallest key    |    largest key     |
-//    +-------------+-----------+--------------+-------------+--------------------+--------------------+
-//    |  Varint64   | Varint64  |   Varint64   |   Varint32  | Varint32 + key_len | Varint32 + key_len |
-//    +-------------+-----------+--------------+-------------+--------------------+--------------------+
+//    +-------------+-----------+--------------+------------+----------------+----------------+
+//    | file number | file size | file entries | file level |  smallest key  |  largest key   |
+//    +-------------+-----------+--------------+------------+----------------+----------------+
+//    |  Varint64   | Varint64  |   Varint64   |  Varint32  | Varint32 + len | Varint32 + len |
+//    +-------------+-----------+--------------+------------+----------------+----------------+
 //
 // The blob file meta is stored in Titan's manifest for quick constructing of
 // meta infomations of all the blob files in memory.
