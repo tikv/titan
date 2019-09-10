25e7efb860656a36db7e31947190717342cc69c4
Wu Jiayu <wujy.cs@gmail.com>
2019-09-10 13:40:11 +0800

    check decoded key is not empty

--- src/blob_format.cc
+++ src/blob_format.cc
@@ -154,12 +154,12 @@ Status BlobFileMeta::DecodeFrom(Slice* src) {
     return Status::Corruption("BlobFileMeta decode failed");
   }
   Slice str;
-  if (GetLengthPrefixedSlice(src, &str) && str!="") {
+  if (GetLengthPrefixedSlice(src, &str) && !str.empty()) {
     smallest_key_.assign(str.data(), str.size());
   } else {
     return Status::Corruption("BlobSmallestKey Decode failed");
   }
-  if (GetLengthPrefixedSlice(src, &str) && str!="") {
+  if (GetLengthPrefixedSlice(src, &str) && !str.empty()) {
     largest_key_.assign(str.data(), str.size());
   } else {
     return Status::Corruption("BlobLargestKey decode failed");
