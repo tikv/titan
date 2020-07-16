99cc009d7b5a67241d745620792e0fbc1251d88a
DorianZheng <xingzhengde72@gmail.com>
2020-07-16 22:52:07 +0800

    bug fix
    Signed-off-by: DorianZheng <xingzhengde72@gmail.com>

--- src/compaction_filter.h
+++ src/compaction_filter.h
@@ -157,6 +157,9 @@ class TitanCompactionFilterFactory final : public CompactionFilterFactory {
           original_filter_factory_->CreateCompactionFilter(context);
       original_filter = original_filter_from_factory.get();
     }
+
+    if (UNLIKELY(original_filter == nullptr)) return nullptr;
+
     return std::unique_ptr<CompactionFilter>(new TitanCompactionFilter(
         titan_db_impl_, cf_name_, original_filter,
         std::move(original_filter_from_factory), blob_storage, skip_value_));
