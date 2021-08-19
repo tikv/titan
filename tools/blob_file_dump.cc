1bbf13334cdb9780e7e3bb0807fb7faeebbb0122
Yi Wu <yiwu@pingcap.com>
2021-08-18 20:20:59 -0700

    fix. update blob_file_dump tool
    Signed-off-by: Yi Wu <yiwu@pingcap.com>

--- tools/blob_file_dump.cc
+++ tools/blob_file_dump.cc
@@ -8,8 +8,6 @@ using GFLAGS_NAMESPACE::ParseCommandLineFlags;
 using GFLAGS_NAMESPACE::SetUsageMessage;
 
 DEFINE_string(path, "", "Path of blob file.");
-DEFINE_uint64(size, 0, "blob file size");
-DEFINE_uint64(number, 0, "blob file number");
 DEFINE_bool(dump, false, "");
 
 #define handle_error(s, location)                                           \
@@ -24,29 +22,31 @@ namespace titandb {
 int blob_file_dump() {
   Env* env = Env::Default();
   Status s;
+    
+  std::string file_name = FLAGS_path;
+  uint64_t file_size = 0;
+  s = env->GetFileSize(file_name, &file_size);
+  handle_error(s, "getting file size");
 
   std::unique_ptr<RandomAccessFileReader> file;
   std::unique_ptr<RandomAccessFile> f;
-  std::string file_name = FLAGS_path;
   s = env->NewRandomAccessFile(file_name, &f, EnvOptions());
   handle_error(s, "open file");
   file.reset(new RandomAccessFileReader(std::move(f), file_name));
 
   std::unique_ptr<BlobFileIterator> iter(new BlobFileIterator(
-      std::move(file), FLAGS_number, FLAGS_size, TitanCFOptions()));
+      std::move(file), 1/*fake file number*/, file_size, TitanCFOptions()));
 
-  printf("seek\n");
   iter->SeekToFirst();
   while (iter->Valid()) {
     handle_error(iter->status(), "status");
     if (FLAGS_dump) {
       std::string key = iter->key().ToString(true);
       std::string value = iter->value().ToString(true);
-      printf("%s: %s\n", key.c_str(), value.c_str());
+      fprintf(stdout, "%s: %s\n", key.c_str(), value.c_str());
     }
   }
-
-  handle_error(iter->status(), "reader open");
+  handle_error(iter->status(), "reading blob file");
   return 0;
 }
 
