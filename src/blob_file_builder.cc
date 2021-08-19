tikv/titan

commit 54a20a56f6b4a5e6d36ce7cab7bde828e96f25cb

Add dictionary compression support for blob file (#189)
Fixes #118

Briefly I've done following things:
- add compression options in TitanCFOptions
- set flags in header when there is compression dictionary
- add some members(states, sample data, etc.) in BlobFileBuilder for dictionary compression logic
- write the dictionary in meta blocks, when dictionary compression is enabled
- refactor BlobEncoder to support dictionary update

currently for decoding
- update BlobFileReader and BlobFileIterator to return Status::NotSupported if a blob file come with dictionary compression.
- add a test case for BlobFileReader

Issue Number: https://github.com/tikv/tikv/issues/8635

Signed-off-by: hexyoungs <chuxdesign@hotmail.com>

src/blob_file_builder.cc
@@ -6,19 +6,120 @@ namespace titandb {
 BlobFileBuilder::BlobFileBuilder(const TitanDBOptions& db_options,
                                  const TitanCFOptions& cf_options,
                                  WritableFileWriter* file)
-    : cf_options_(cf_options),
+    : builder_state_(cf_options.blob_file_compression_options.max_dict_bytes > 0
+                         ? BuilderState::kBuffered
+                         : BuilderState::kUnbuffered),
+      cf_options_(cf_options),
       file_(file),
-      encoder_(cf_options_.blob_file_compression) {
+      encoder_(cf_options.blob_file_compression,
+               cf_options.blob_file_compression_options) {
+#if ZSTD_VERSION_NUMBER < 10103
+  if (cf_options_.blob_file_compression_options.max_dict_bytes > 0) {
+    status_ = Status::NotSupported("ZSTD version too old.");
+    return;
+  }
+#endif
+  WriteHeader();
+}
+
+void BlobFileBuilder::WriteHeader() {
   BlobFileHeader header;
+  if (cf_options_.blob_file_compression_options.max_dict_bytes > 0) {
+    header.flags |= BlobFileHeader::kHasUncompressionDictionary;
+  }
   std::string buffer;
   header.EncodeTo(&buffer);
   status_ = file_->Append(buffer);
 }
 
-void BlobFileBuilder::Add(const BlobRecord& record, BlobHandle* handle) {
+void BlobFileBuilder::Add(const BlobRecord& record,
+                          std::unique_ptr<BlobRecordContext> ctx,
+                          OutContexts* out_ctx) {
   if (!ok()) return;
+  std::string key = record.key.ToString();
+  if (builder_state_ == BuilderState::kBuffered) {
+    std::string record_str;
+    // Encode to take ownership of underlying string.
+    record.EncodeTo(&record_str);
+    sample_records_.emplace_back(record_str);
+    sample_str_len_ += record_str.size();
+    cached_contexts_.emplace_back(std::move(ctx));
+    if (cf_options_.blob_file_compression_options.zstd_max_train_bytes > 0 &&
+        sample_str_len_ >=
+            cf_options_.blob_file_compression_options.zstd_max_train_bytes) {
+      EnterUnbuffered(out_ctx);
+    }
+  } else {
+    encoder_.EncodeRecord(record);
+    WriteEncoderData(&ctx->new_blob_index.blob_handle);
+    out_ctx->emplace_back(std::move(ctx));
+  }
 
-  encoder_.EncodeRecord(record);
+  // The keys added into blob files are in order.
+  // We do key range checks for both state
+  if (smallest_key_.empty()) {
+    smallest_key_.assign(record.key.data(), record.key.size());
+  }
+  assert(cf_options_.comparator->Compare(record.key, Slice(smallest_key_)) >=
+         0);
+  assert(cf_options_.comparator->Compare(record.key, Slice(largest_key_)) >= 0);
+  largest_key_.assign(record.key.data(), record.key.size());
+}
+
+void BlobFileBuilder::AddSmall(std::unique_ptr<BlobRecordContext> ctx) {
+  cached_contexts_.emplace_back(std::move(ctx));
+}
+
+void BlobFileBuilder::EnterUnbuffered(OutContexts* out_ctx) {
+  // Using collected samples to train the compression dictionary
+  // Then replay those records in memory, encode them to blob file
+  // When above things are done, transform builder state into unbuffered
+  std::string samples;
+  samples.reserve(sample_str_len_);
+  std::vector<size_t> sample_lens;
+
+  for (const auto& record_str : sample_records_) {
+    samples.append(record_str, 0, record_str.size());
+    sample_lens.emplace_back(record_str.size());
+  }
+  std::string dict;
+  dict = ZSTD_TrainDictionary(
+      samples, sample_lens,
+      cf_options_.blob_file_compression_options.max_dict_bytes);
+
+  compression_dict_.reset(
+      new CompressionDict(dict, cf_options_.blob_file_compression,
+                          cf_options_.blob_file_compression_options.level));
+  encoder_.SetCompressionDict(compression_dict_.get());
+
+  FlushSampleRecords(out_ctx);
+
+  builder_state_ = BuilderState::kUnbuffered;
+}
+
+void BlobFileBuilder::FlushSampleRecords(OutContexts* out_ctx) {
+  assert(cached_contexts_.size() >= sample_records_.size());
+  size_t sample_idx = 0, ctx_idx = 0;
+  for (; sample_idx < sample_records_.size(); sample_idx++, ctx_idx++) {
+    const std::string& record_str = sample_records_[sample_idx];
+    for (; ctx_idx < cached_contexts_.size() &&
+           cached_contexts_[ctx_idx]->has_value;
+         ctx_idx++) {
+      out_ctx->emplace_back(std::move(cached_contexts_[ctx_idx]));
+    }
+    const std::unique_ptr<BlobRecordContext>& ctx = cached_contexts_[ctx_idx];
+    encoder_.EncodeSlice(record_str);
+    WriteEncoderData(&ctx->new_blob_index.blob_handle);
+    out_ctx->emplace_back(std::move(cached_contexts_[ctx_idx]));
+  }
+  assert(sample_idx == sample_records_.size());
+  assert(ctx_idx == cached_contexts_.size());
+  sample_records_.clear();
+  sample_str_len_ = 0;
+  cached_contexts_.clear();
+}
+
+void BlobFileBuilder::WriteEncoderData(BlobHandle* handle) {
   handle->offset = file_->GetFileSize();
   handle->size = encoder_.GetEncodedSize();
   live_data_size_ += handle->size;
@@ -27,23 +128,56 @@ void BlobFileBuilder::Add(const BlobRecord& record, BlobHandle* handle) {
   if (ok()) {
     status_ = file_->Append(encoder_.GetRecord());
     num_entries_++;
-    // The keys added into blob files are in order.
-    if (smallest_key_.empty()) {
-      smallest_key_.assign(record.key.data(), record.key.size());
-    }
-    assert(cf_options_.comparator->Compare(record.key, Slice(smallest_key_)) >=
-           0);
-    assert(cf_options_.comparator->Compare(record.key, Slice(largest_key_)) >=
-           0);
-    largest_key_.assign(record.key.data(), record.key.size());
   }
 }
 
-Status BlobFileBuilder::Finish() {
+void BlobFileBuilder::WriteRawBlock(const Slice& block, BlockHandle* handle) {
+  handle->set_offset(file_->GetFileSize());
+  handle->set_size(block.size());
+  status_ = file_->Append(block);
+  if (ok()) {
+    // follow rocksdb's block based table format
+    char trailer[kBlockTrailerSize];
+    // only compression dictionary and meta index block are written
+    // by this method, we use `kNoCompression` as placeholder
+    trailer[0] = kNoCompression;
+    char* trailer_without_type = trailer + 1;
+
+    // crc32 checksum
+    auto crc = crc32c::Value(block.data(), block.size());
+    crc = crc32c::Extend(crc, trailer, 1);  // Extend to cover compression type
+    EncodeFixed32(trailer_without_type, crc32c::Mask(crc));
+    status_ = file_->Append(Slice(trailer, kBlockTrailerSize));
+  }
+}
+
+void BlobFileBuilder::WriteCompressionDictBlock(
+    MetaIndexBuilder* meta_index_builder) {
+  BlockHandle handle;
+  WriteRawBlock(compression_dict_->GetRawDict(), &handle);
+  if (ok()) {
+    meta_index_builder->Add(kCompressionDictBlock, handle);
+  }
+}
+
+Status BlobFileBuilder::Finish(OutContexts* out_ctx) {
   if (!ok()) return status();
 
-  std::string buffer;
+  if (builder_state_ == BuilderState::kBuffered) {
+    EnterUnbuffered(out_ctx);
+  }
+
   BlobFileFooter footer;
+  // if has compression dictionary, encode it into meta blocks
+  if (cf_options_.blob_file_compression_options.max_dict_bytes > 0) {
+    BlockHandle meta_index_handle;
+    MetaIndexBuilder meta_index_builder;
+    WriteCompressionDictBlock(&meta_index_builder);
+    WriteRawBlock(meta_index_builder.Finish(), &meta_index_handle);
+    footer.meta_index_handle = meta_index_handle;
+  }
+
+  std::string buffer;
   footer.EncodeTo(&buffer);
 
   status_ = file_->Append(buffer);

src/blob_file_builder.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include "blob_format.h"
+#include "table/meta_blocks.h"
 #include "titan/options.h"
+#include "util/autovector.h"
+#include "util/compression.h"
 #include "util/file_reader_writer.h"
 
 namespace rocksdb {
@@ -30,24 +33,65 @@ namespace titandb {
 // meta index block with block handles pointed to the meta blocks. The
 // meta block and the meta index block are formatted the same as the
 // BlockBasedTable.
-
 class BlobFileBuilder {
  public:
+  // States of the builder.
+  //
+  // - `kBuffered`: This is the initial state where zero or more data blocks are
+  //   accumulated uncompressed in-memory. From this state, call
+  //   `EnterUnbuffered()` to finalize the compression dictionary if enabled,
+  //   compress/write out any buffered blocks, and proceed to the `kUnbuffered`
+  //   state.
+  //
+  // - `kUnbuffered`: This is the state when compression dictionary is finalized
+  //   either because it wasn't enabled in the first place or it's been created
+  //   from sampling previously buffered data. In this state, blocks are simply
+  //   compressed/written out as they fill up. From this state, call `Finish()`
+  //   to complete the file (write meta-blocks, etc.), or `Abandon()` to delete
+  //   the partially created file.
+  enum class BuilderState {
+    kBuffered,
+    kUnbuffered,
+  };
+
+  struct BlobRecordContext {
+    std::string key;  // original internal key
+    BlobIndex original_blob_index;
+    BlobIndex new_blob_index;
+    bool has_value = false;
+    std::string value;
+  };
+  typedef autovector<std::unique_ptr<BlobRecordContext>> OutContexts;
+
   // Constructs a builder that will store the contents of the file it
   // is building in "*file". Does not close the file. It is up to the
   // caller to sync and close the file after calling Finish().
   BlobFileBuilder(const TitanDBOptions& db_options,
                   const TitanCFOptions& cf_options, WritableFileWriter* file);
 
-  // Adds the record to the file and points the handle to it.
-  void Add(const BlobRecord& record, BlobHandle* handle);
+  // Tries to add the record to the file
+  // Notice:
+  // 1. The `out_ctx` might be empty when builder is in `kBuffered` state.
+  // 2. Caller should set `ctx.new_blob_index.file_number` before pass it in,
+  //    the file builder will only change the `blob_handle` of it
+  void Add(const BlobRecord& record, std::unique_ptr<BlobRecordContext> ctx,
+           OutContexts* out_ctx);
+
+  // AddSmall is used to prevent the disorder issue, small KV pairs and blob
+  // index block may be passed in here
+  void AddSmall(std::unique_ptr<BlobRecordContext> ctx);
+
+  // Returns builder state
+  BuilderState GetBuilderState() { return builder_state_; }
 
   // Returns non-ok iff some error has been detected.
   Status status() const { return status_; }
 
-  // Finishes building the table.
+  // Finishes building the table, and return status.
+  // This method will return modify output contexts when it is called in
+  // `kBuffered` state.
   // REQUIRES: Finish(), Abandon() have not been called.
-  Status Finish();
+  Status Finish(OutContexts* out_ctx);
 
   // Abandons building the table. If the caller is not going to call
   // Finish(), it must call Abandon() before destroying this builder.
@@ -63,13 +107,31 @@ class BlobFileBuilder {
   uint64_t live_data_size() const { return live_data_size_; }
 
  private:
+  BuilderState builder_state_;
+
   bool ok() const { return status().ok(); }
+  // Enter unbuffered state, only be called after collecting enough samples
+  // for compression dictionary. It will modify `out_ctx` of the buffered
+  // records
+  void EnterUnbuffered(OutContexts* out_ctx);
+  void WriteHeader();
+  void WriteRawBlock(const Slice& block, BlockHandle* handle);
+  void WriteCompressionDictBlock(MetaIndexBuilder* meta_index_builder);
+  void FlushSampleRecords(OutContexts* out_ctx);
+  void WriteEncoderData(BlobHandle* handle);
 
   TitanCFOptions cf_options_;
   WritableFileWriter* file_;
 
   Status status_;
   BlobEncoder encoder_;
+
+  // following 3 may be refactored in to Rep
+  std::vector<std::string> sample_records_;
+  uint64_t sample_str_len_ = 0;
+  std::unique_ptr<CompressionDict> compression_dict_;
+
+  OutContexts cached_contexts_;
 
   uint64_t num_entries_ = 0;
   std::string smallest_key_;

src/blob_file_iterator.cc
@@ -28,6 +28,10 @@ bool BlobFileIterator::Init() {
   }
   BlobFileHeader blob_file_header;
   status_ = blob_file_header.DecodeFrom(&slice);
+  if (blob_file_header.flags & BlobFileHeader::kHasUncompressionDictionary) {
+    status_ = Status::NotSupported(
+        "blob file with dictionary compression is not supported yet");
+  }
   if (!status_.ok()) {
     return false;
   }

src/blob_file_reader.cc
@@ -8,10 +8,9 @@
 
 #include "file/filename.h"
 #include "test_util/sync_point.h"
+#include "titan_stats.h"
 #include "util/crc32c.h"
 #include "util/string_util.h"
-
-#include "titan_stats.h"
 
 namespace rocksdb {
 namespace titandb {
@@ -64,9 +63,19 @@ Status BlobFileReader::Open(const TitanCFOptions& options,
     return Status::Corruption("file is too short to be a blob file");
   }
 
+  BlobFileHeader header;
+  Status s = ReadHeader(file, &header);
+  if (!s.ok()) {
+    return s;
+  }
+  if (header.flags & BlobFileHeader::kHasUncompressionDictionary) {
+    return Status::NotSupported(
+        "blob file with dictionary compression is not supported yet");
+  }
+
   FixedSlice<BlobFileFooter::kEncodedLength> buffer;
-  Status s = file->Read(file_size - BlobFileFooter::kEncodedLength,
-                        BlobFileFooter::kEncodedLength, &buffer, buffer.get());
+  s = file->Read(file_size - BlobFileFooter::kEncodedLength,
+                 BlobFileFooter::kEncodedLength, &buffer, buffer.get());
   if (!s.ok()) {
     return s;
   }
@@ -81,6 +90,18 @@ Status BlobFileReader::Open(const TitanCFOptions& options,
   reader->footer_ = footer;
   result->reset(reader);
   return Status::OK();
+}
+
+Status BlobFileReader::ReadHeader(std::unique_ptr<RandomAccessFileReader>& file,
+                                  BlobFileHeader* header) {
+  FixedSlice<BlobFileHeader::kMaxEncodedLength> buffer;
+  Status s =
+      file->Read(0, BlobFileHeader::kMaxEncodedLength, &buffer, buffer.get());
+  if (!s.ok()) return s;
+
+  s = DecodeInto(buffer, header);
+
+  return s;
 }
 
 BlobFileReader::BlobFileReader(const TitanCFOptions& options,

src/blob_format.h
@@ -61,15 +61,32 @@ struct BlobRecord {
 
 class BlobEncoder {
  public:
-  BlobEncoder(CompressionType compression,
-              const CompressionDict& compression_dict)
-      : compression_ctx_(compression),
-        compression_info_(compression_opt_, compression_ctx_, compression_dict,
-                          compression, 0 /*sample_for_compression*/) {}
+  BlobEncoder(CompressionType compression, CompressionOptions compression_opt,
+              const CompressionDict* compression_dict)
+      : compression_opt_(compression_opt),
+        compression_ctx_(compression),
+        compression_dict_(compression_dict),
+        compression_info_(new CompressionInfo(
+            compression_opt_, compression_ctx_, *compression_dict_, compression,
+            0 /*sample_for_compression*/)) {}
   BlobEncoder(CompressionType compression)
-      : BlobEncoder(compression, CompressionDict::GetEmptyDict()) {}
+      : BlobEncoder(compression, CompressionOptions(),
+                    &CompressionDict::GetEmptyDict()) {}
+  BlobEncoder(CompressionType compression,
+              const CompressionDict* compression_dict)
+      : BlobEncoder(compression, CompressionOptions(), compression_dict) {}
+  BlobEncoder(CompressionType compression, CompressionOptions compression_opt)
+      : BlobEncoder(compression, compression_opt,
+                    &CompressionDict::GetEmptyDict()) {}
 
   void EncodeRecord(const BlobRecord& record);
+  void EncodeSlice(const Slice& record);
+  void SetCompressionDict(const CompressionDict* compression_dict) {
+    compression_dict_ = compression_dict;
+    compression_info_.reset(new CompressionInfo(
+        compression_opt_, compression_ctx_, *compression_dict_,
+        compression_info_->type(), compression_info_->SampleForCompression()));
+  }
 
   Slice GetHeader() const { return Slice(header_, sizeof(header_)); }
   Slice GetRecord() const { return record_; }
@@ -83,7 +100,8 @@ class BlobEncoder {
   std::string compressed_buffer_;
   CompressionOptions compression_opt_;
   CompressionContext compression_ctx_;
-  CompressionInfo compression_info_;
+  const CompressionDict* compression_dict_;
+  std::unique_ptr<CompressionInfo> compression_info_;
 };
 
 class BlobDecoder {
@@ -352,10 +370,6 @@ struct BlobFileFooter {
   static const uint64_t kEncodedLength{kBlobFooterSize};
 
   BlockHandle meta_index_handle{BlockHandle::NullBlockHandle()};
-
-  // Points to a uncompression dictionary (which is also pointed to by the meta
-  // index) when `kHasUncompressionDictionary` is set in the header.
-  BlockHandle uncompression_dict_handle{BlockHandle::NullBlockHandle()};
 
   void EncodeTo(std::string* dst) const;
   Status DecodeFrom(Slice* src);

commit 48754f312995a4fb649f4b93ff7ce3cb0255e0f2

Fix false corruption error when reading from v1 blob file (#221)
To support dictionary compression we added a v2 blob file header to store extra flags. However, the check in BlobFileReader , when reading a v1 blob file, falsely asserted the file header is shorter than expected:
https://github.com/tikv/titan/blob/ecb5cba016096309cbd9b566bdc4ce62307527a0/src/blob_file_reader.cc#L103
https://github.com/tikv/titan/blob/ecb5cba016096309cbd9b566bdc4ce62307527a0/src/blob_format.h#L394
Refactoring the code to bypass the check to fix the issue.

The issue is introduced in https://github.com/tikv/titan/pull/189. TiKV is affected when Titan is enabled and upgrade from pre-5.0 versions to >=5.0.0 versions. It will make TiKV fall in crash loop.

Also adding a titan_blob_file_dump tool to dump blob file content.

Signed-off-by: Yi Wu <yiwu@pingcap.com>

CMakeLists.txt
@@ -156,6 +156,10 @@ if (WITH_TITAN_TOOLS)
   add_executable(titan_manifest_dump tools/manifest_dump.cc)
   target_include_directories(titan_manifest_dump PRIVATE ${gflags_INCLUDE_DIR})
   target_link_libraries(titan_manifest_dump ${TOOLS_LIBS})
+  
+  add_executable(titan_blob_file_dump tools/blob_file_dump.cc)
+  target_include_directories(titan_blob_file_dump PRIVATE ${gflags_INCLUDE_DIR})
+  target_link_libraries(titan_blob_file_dump ${TOOLS_LIBS})
 endif()
 
 # Installation - copy lib/ and include/

src/blob_file_builder.cc
@@ -5,26 +5,38 @@ namespace titandb {
 
 BlobFileBuilder::BlobFileBuilder(const TitanDBOptions& db_options,
                                  const TitanCFOptions& cf_options,
-                                 WritableFileWriter* file)
+                                 WritableFileWriter* file,
+                                 uint32_t blob_file_version)
     : builder_state_(cf_options.blob_file_compression_options.max_dict_bytes > 0
                          ? BuilderState::kBuffered
                          : BuilderState::kUnbuffered),
       cf_options_(cf_options),
       file_(file),
+      blob_file_version_(blob_file_version),
       encoder_(cf_options.blob_file_compression,
                cf_options.blob_file_compression_options) {
-#if ZSTD_VERSION_NUMBER < 10103
-  if (cf_options_.blob_file_compression_options.max_dict_bytes > 0) {
-    status_ = Status::NotSupported("ZSTD version too old.");
+  status_ = BlobFileHeader::ValidateVersion(blob_file_version_);
+  if (!status_.ok()) {
     return;
   }
+  if (cf_options_.blob_file_compression_options.max_dict_bytes > 0) {
+    if (blob_file_version_ != BlobFileHeader::kVersion2) {
+      status_ = Status::NotSupported(
+          "dictionary comparession is not supported by blob file version 1");
+    }
+#if ZSTD_VERSION_NUMBER < 10103
+    status_ = Status::NotSupported("ZSTD version too old.");
+    return;
 #endif
+  }
   WriteHeader();
 }
 
 void BlobFileBuilder::WriteHeader() {
   BlobFileHeader header;
+  header.version = blob_file_version_;
   if (cf_options_.blob_file_compression_options.max_dict_bytes > 0) {
+    assert(blob_file_version_ == BlobFileHeader::kVersion2);
     header.flags |= BlobFileHeader::kHasUncompressionDictionary;
   }
   std::string buffer;
@@ -174,6 +186,7 @@ Status BlobFileBuilder::Finish(OutContexts* out_ctx) {
   BlobFileFooter footer;
   // if has compression dictionary, encode it into meta blocks
   if (cf_options_.blob_file_compression_options.max_dict_bytes > 0) {
+    assert(blob_file_version_ == BlobFileHeader::kVersion2);
     BlockHandle meta_index_handle;
     MetaIndexBuilder meta_index_builder;
     WriteCompressionDictBlock(&meta_index_builder);

src/blob_file_builder.h
@@ -67,7 +67,8 @@ class BlobFileBuilder {
   // is building in "*file". Does not close the file. It is up to the
   // caller to sync and close the file after calling Finish().
   BlobFileBuilder(const TitanDBOptions& db_options,
-                  const TitanCFOptions& cf_options, WritableFileWriter* file);
+                  const TitanCFOptions& cf_options, WritableFileWriter* file,
+                  uint32_t blob_file_version = BlobFileHeader::kVersion2);
 
   // Tries to add the record to the file
   // Notice:
@@ -124,6 +125,7 @@ class BlobFileBuilder {
 
   TitanCFOptions cf_options_;
   WritableFileWriter* file_;
+  const uint32_t blob_file_version_;
 
   Status status_;
   BlobEncoder encoder_;

src/blob_file_iterator.cc
@@ -28,7 +28,7 @@ bool BlobFileIterator::Init() {
     return false;
   }
   BlobFileHeader blob_file_header;
-  status_ = blob_file_header.DecodeFrom(&slice);
+  status_ = DecodeInto(slice, &blob_file_header, true /*ignore_extra_bytes*/);
   if (!status_.ok()) {
     return false;
   }

src/blob_file_reader.cc
@@ -125,7 +125,7 @@ Status BlobFileReader::ReadHeader(std::unique_ptr<RandomAccessFileReader>& file,
       file->Read(0, BlobFileHeader::kMaxEncodedLength, &buffer, buffer.get());
   if (!s.ok()) return s;
 
-  s = DecodeInto(buffer, header);
+  s = DecodeInto(buffer, header, true /*ignore_extra_bytes*/);
 
   return s;
 }

src/blob_format.h
@@ -352,6 +352,14 @@ struct BlobFileHeader {
   uint32_t version = kVersion2;
   uint32_t flags = 0;
 
+  static Status ValidateVersion(uint32_t ver) {
+    if (ver != BlobFileHeader::kVersion1 && ver != BlobFileHeader::kVersion2) {
+      return Status::InvalidArgument("unrecognized blob file version " +
+                                     ToString(ver));
+    }
+    return Status::OK();
+  }
+
   uint64_t size() const {
     return version == BlobFileHeader::kVersion1
                ? BlobFileHeader::kMinEncodedLength
@@ -387,11 +395,12 @@ struct BlobFileFooter {
 
 // A convenient template to decode a const slice.
 template <typename T>
-Status DecodeInto(const Slice& src, T* target) {
-  auto tmp = src;
-  auto s = target->DecodeFrom(&tmp);
-  if (s.ok() && !tmp.empty()) {
-    s = Status::Corruption(Slice());
+Status DecodeInto(const Slice& src, T* target,
+                  bool ignore_extra_bytes = false) {
+  Slice tmp = src;
+  Status s = target->DecodeFrom(&tmp);
+  if (!ignore_extra_bytes && s.ok() && !tmp.empty()) {
+    s = Status::Corruption("redundant bytes when decoding blob file");
   }
   return s;
 }

tools/blob_file_dump.cc
@@ -1,0 +1,61 @@
+// Copyright 2021-present TiKV Project Authors. Licensed under Apache-2.0.
+
+#include "blob_file_iterator.h"
+#include "file/filename.h"
+#include "util/gflags_compat.h"
+
+using GFLAGS_NAMESPACE::ParseCommandLineFlags;
+using GFLAGS_NAMESPACE::SetUsageMessage;
+
+DEFINE_string(path, "", "Path of blob file.");
+DEFINE_bool(dump, false, "");
+
+#define handle_error(s, location)                                           \
+  if (!s.ok()) {                                                            \
+    fprintf(stderr, "error when %s: %s\n", location, s.ToString().c_str()); \
+    return 1;                                                               \
+  }
+
+namespace rocksdb {
+namespace titandb {
+
+int blob_file_dump() {
+  Env* env = Env::Default();
+  Status s;
+
+  std::string file_name = FLAGS_path;
+  uint64_t file_size = 0;
+  s = env->GetFileSize(file_name, &file_size);
+  handle_error(s, "getting file size");
+
+  std::unique_ptr<RandomAccessFileReader> file;
+  std::unique_ptr<RandomAccessFile> f;
+  s = env->NewRandomAccessFile(file_name, &f, EnvOptions());
+  handle_error(s, "open file");
+  file.reset(new RandomAccessFileReader(std::move(f), file_name));
+
+  std::unique_ptr<BlobFileIterator> iter(new BlobFileIterator(
+      std::move(file), 1 /*fake file number*/, file_size, TitanCFOptions()));
+
+  iter->SeekToFirst();
+  while (iter->Valid()) {
+    handle_error(iter->status(), "status");
+    if (FLAGS_dump) {
+      std::string key = iter->key().ToString(true);
+      std::string value = iter->value().ToString(true);
+      fprintf(stdout, "%s: %s\n", key.c_str(), value.c_str());
+    }
+  }
+  handle_error(iter->status(), "reading blob file");
+  return 0;
+}
+
+}  // namespace titandb
+}  // namespace rocksdb
+
+int main(int argc, char** argv) {
+  SetUsageMessage(std::string("\nUSAGE\n") + std::string(argv[0]) +
+                  " [OPTIONS]...");
+  ParseCommandLineFlags(&argc, &argv, true);
+  return rocksdb::titandb::blob_file_dump();
+}

src/blob_file_test.cc
@@ -50,7 +50,8 @@ class BlobFileTest : public testing::Test {
     return s;
   }
 
-  void TestBlobFilePrefetcher(TitanOptions options) {
+  void TestBlobFilePrefetcher(TitanOptions options,
+                              uint32_t blob_file_version = 0) {
     options.dirname = dirname_;
     TitanDBOptions db_options(options);
     TitanCFOptions cf_options(options);
@@ -66,8 +67,15 @@ class BlobFileTest : public testing::Test {
       file.reset(
           new WritableFileWriter(std::move(f), file_name_, env_options_));
     }
-    std::unique_ptr<BlobFileBuilder> builder(
-        new BlobFileBuilder(db_options, cf_options, file.get()));
+    std::unique_ptr<BlobFileBuilder> builder;
+    if (blob_file_version == 0) {
+      // Default blob file version
+      builder.reset(new BlobFileBuilder(db_options, cf_options, file.get()));
+    } else {
+      // Test with specific blob file version
+      builder.reset(new BlobFileBuilder(db_options, cf_options, file.get(),
+                                        blob_file_version));
+    }
 
     for (int i = 0; i < n; i++) {
       auto key = GenKey(i);
@@ -115,7 +123,8 @@ class BlobFileTest : public testing::Test {
     }
   }
 
-  void TestBlobFileReader(TitanOptions options) {
+  void TestBlobFileReader(TitanOptions options,
+                          uint32_t blob_file_version = 0) {
     options.dirname = dirname_;
     TitanDBOptions db_options(options);
     TitanCFOptions cf_options(options);
@@ -131,8 +140,16 @@ class BlobFileTest : public testing::Test {
       file.reset(
           new WritableFileWriter(std::move(f), file_name_, env_options_));
     }
-    std::unique_ptr<BlobFileBuilder> builder(
-        new BlobFileBuilder(db_options, cf_options, file.get()));
+
+    std::unique_ptr<BlobFileBuilder> builder;
+    if (blob_file_version == 0) {
+      // Default blob file version
+      builder.reset(new BlobFileBuilder(db_options, cf_options, file.get()));
+    } else {
+      // Test with specific blob file version
+      builder.reset(new BlobFileBuilder(db_options, cf_options, file.get(),
+                                        blob_file_version));
+    }
 
     for (int i = 0; i < n; i++) {
       auto key = GenKey(i);
@@ -197,6 +214,7 @@ class BlobFileTest : public testing::Test {
 TEST_F(BlobFileTest, BlobFileReader) {
   TitanOptions options;
   TestBlobFileReader(options);
+  TestBlobFileReader(options, BlobFileHeader::kVersion1);
   options.blob_file_compression = kLZ4Compression;
   TestBlobFileReader(options);
 }
@@ -204,6 +222,7 @@ TEST_F(BlobFileTest, BlobFileReader) {
 TEST_F(BlobFileTest, BlobFilePrefetcher) {
   TitanOptions options;
   TestBlobFilePrefetcher(options);
+  TestBlobFilePrefetcher(options, BlobFileHeader::kVersion1);
   options.blob_cache = NewLRUCache(1 << 20);
   TestBlobFilePrefetcher(options);
   options.blob_file_compression = kLZ4Compression;