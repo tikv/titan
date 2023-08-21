24333a93090f1014433b0453634036c7b705e461
Connor1996 <zbk602423539@gmail.com>
2023-08-21 10:53:13 +0800

    fix shawdow warning
    Signed-off-by: Connor1996 <zbk602423539@gmail.com>

--- src/blob_file_set.cc
+++ src/blob_file_set.cc
@@ -217,9 +217,9 @@ struct BlobFileSet::ManifestWriter {
   EditCollector& collector;
   VersionEdit& edit;
 
-  explicit ManifestWriter(port::Mutex* mu, EditCollector& collector,
-                          VersionEdit& edit)
-      : done(false), cv(mu), collector(collector), edit(edit) {}
+  explicit ManifestWriter(port::Mutex* mu, EditCollector& _collector,
+                          VersionEdit& _edit)
+      : done(false), cv(mu), collector(_collector), edit(_edit) {}
 };
 
 Status BlobFileSet::LogAndApply(VersionEdit& edit) {
