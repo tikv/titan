cd995de17607fe9ecdd0450afa28ba67d12c7835
Yang Zhang <yang.zhang@pingcap.com>
2024-07-29 11:11:20 -0700

    Use default value
    Signed-off-by: Yang Zhang <yang.zhang@pingcap.com>

--- src/blob_file_reader.h
+++ src/blob_file_reader.h
@@ -28,12 +28,8 @@ class BlobFileReader {
   // of the record is stored in the value slice underlying, so the value slice
   // must be valid when the record is used.
   Status Get(const ReadOptions& options, const BlobHandle& handle,
-             BlobRecord* record, OwnedSlice* buffer) {
-    return Get(options, handle, record, buffer, false);
-  }
-
-  Status Get(const ReadOptions& options, const BlobHandle& handle,
-             BlobRecord* record, OwnedSlice* buffer, bool for_compaction);
+             BlobRecord* record, OwnedSlice* buffer,
+             bool for_compaction = false);
 
  private:
   friend class BlobFilePrefetcher;
